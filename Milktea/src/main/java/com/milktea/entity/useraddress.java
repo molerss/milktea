46eb86d312a1483f942c0bf2edc549fc74fe6b96
yang <yang@qq.com>
2022-09-25 12:39:11 +0800

    地址信息实体类

---
+++ Milktea/src/main/java/com/milktea/entity/useraddress.java
@@ -0,0 +1,28 @@
+package com.milktea.entity;
+
+import lombok.Data;
+
+@Data
+public class useraddress {
+
+    //    地址id
+    private Integer id;
+
+    //    用户id
+    private Integer user_id;
+
+    //    用户姓名
+    private String name;
+
+    //    用户性别
+    private String sex;
+
+    //    用户地址
+    private String address;
+
+    //    用户电话
+    private String phone;
+
+    //    地址标签
+    private String code;
+}
