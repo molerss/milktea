89a0400fefe13f0018ebb02cace25cf420688c12
yang <yang@qq.com>
2022-09-25 14:50:59 +0800

    用户实体类

---
+++ Milktea/src/main/java/com/milktea/entity/Shoppingcart.java
@@ -0,0 +1,19 @@
+package com.milktea.entity;
+
+import lombok.Data;
+
+/**
+ * 购物车
+ */
+@Data
+public class Shoppingcart {
+
+//    购物车id
+    private Integer id;
+
+//    用户id
+    private Integer userId;
+
+//    商品id
+    private Integer goodsId;
+}
