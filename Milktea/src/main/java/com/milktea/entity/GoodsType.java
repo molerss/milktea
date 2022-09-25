2757e792a427f0747ecf457ee9d81556be7952ca
yang <yang@qq.com>
2022-09-25 14:55:39 +0800

    商品类型实体类

---
+++ Milktea/src/main/java/com/milktea/entity/GoodsType.java
@@ -0,0 +1,19 @@
+package com.milktea.entity;
+
+import lombok.Data;
+
+/**
+ * 商品类型表
+ */
+@Data
+public class GoodsType {
+
+//    id
+    private Integer id;
+
+//    类型名
+    private String typename;
+
+//    类型内容
+    private String typecontent;
+}
