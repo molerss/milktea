672f9280e8304799cd9a8525508e28c7cea7e0f6
yang <yang@qq.com>
2022-09-25 14:58:02 +0800

    商品-配料实体类

---
+++ Milktea/src/main/java/com/milktea/entity/GoodsPeiliao.java
@@ -0,0 +1,19 @@
+package com.milktea.entity;
+
+import lombok.Data;
+
+/**
+ * 商品-配料表
+ */
+@Data
+public class GoodsPeiliao {
+
+//    id
+    private Integer id;
+
+//    商品id
+    private Integer goodsId;
+
+//    配料id
+    private Integer peiliaoId;
+}
