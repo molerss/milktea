b8f755d0638e445e9f02708df25a91e32f4013fb
yang <yang@qq.com>
2022-10-04 16:51:35 +0800

    自动填入数据

---
+++ Milktea/src/main/java/com/milktea/common/MyMetaObjecthandler.java
@@ -0,0 +1,22 @@
+package com.milktea.common;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+
+import java.time.LocalDateTime;
+
+public class MyMetaObjecthandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        metaObject.setValue("createTime", LocalDateTime.now());
+        metaObject.setValue("updateTime", LocalDateTime.now());
+        metaObject.setValue("createUser", BaseContext.getCurrentId());
+        metaObject.setValue("updateUser", BaseContext.getCurrentId());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        metaObject.setValue("updateTime",LocalDateTime.now());
+        metaObject.setValue("updateUser",BaseContext.getCurrentId());
+    }
+}
