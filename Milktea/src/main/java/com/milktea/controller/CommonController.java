ad104973aee6a8399f8444c892c040e134730e2e
yang <yang@qq.com>
2022-11-08 11:10:12 +0800

    增加了商品图片上传下载方法

---
+++ Milktea/src/main/java/com/milktea/controller/CommonController.java
@@ -0,0 +1,94 @@
+package com.milktea.controller;
+
+
+import com.milktea.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 文件上传和下载
+ */
+@RestController
+@RequestMapping("/common")
+@Slf4j
+public class CommonController {
+
+    @Value("${milktea.path}")
+    private String basePath;
+
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @PostMapping ("/upload")
+    public R<String> upload(MultipartFile file){
+        //file是一个临时文件，需要储存到指定位置，不然请求完成后就自动删除
+        log.info(file.toString());
+
+        //原始文件名
+        String originalFilename = file.getOriginalFilename();
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+        //使用UUID从新生成文件名，防止文件名重复造成文件覆盖
+        String fileName = UUID.randomUUID().toString() + suffix;
+
+        //创建一个目录文件
+        File dir = new File(basePath);
+
+        //判断当前目录是否存在
+        if (!dir.exists()){
+            //目录不存在，需要创建
+            dir.mkdirs();
+        }
+
+        try {
+            file.transferTo(new File(basePath + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return R.success(fileName);
+    }
+
+
+    @GetMapping("/download")
+    public void dowonload(String name, HttpServletResponse response){
+        //输入流，通过输入流读取文件内容
+        try {
+            //输入流，通过输入流读取文件内容
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+
+            //输出流，通过输出流将文件写回浏览器，在浏览器展示图片
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            response.setContentType("image/jpeg");
+
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = fileInputStream.read(bytes)) != -1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+
+            //关闭资源
+            outputStream.close();
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
