6052909df58cd95556f3f638bc4b6ac431061e47
yang <yang@qq.com>
2022-11-09 09:37:38 +0800

    增加了时间自动填充方法

--- Milktea/src/main/java/com/milktea/common/MyMetaObjecthandler.java
+++ Milktea/src/main/java/com/milktea/common/MyMetaObjecthandler.java
@@ -2,9 +2,11 @@ package com.milktea.common;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+@Component
 public class MyMetaObjecthandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
